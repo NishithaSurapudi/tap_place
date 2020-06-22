74e1f82d265635c9a94e0d90d90bc3332b8dac44
demoUser+1-at-132614107947 <837173@cognizant.com>
2020-06-22 16:03:33 +0530

    tried individual js

---
+++ camera.js
@@ -0,0 +1,22 @@
+        // Component that places model where the ground is clicked
+        AFRAME.registerComponent('tap-place', {
+            init: function() {
+                const ground = document.getElementById('ground')
+                ground.addEventListener('click', event => {
+                    // Create new entity for the new object
+                    const newElement = document.createElement('a-entity')
+
+                    // The raycaster gives a location of the touch in the scene
+                    const touchPoint = event.detail.intersection.point
+                    newElement.setAttribute('position', touchPoint)
+                    newElement.setAttribute('visible', 'false')
+                    newElement.setAttribute('scale', '0.5 0.5 0.5')
+                    newElement.setAttribute('gltf-model', '#Catapult')
+                    this.el.sceneEl.appendChild(newElement)
+                    newElement.addEventListener('model-loaded', () => {
+                        newElement.setAttribute('visible', 'true')
+                        
+                    })
+                })
+            }
+        })
