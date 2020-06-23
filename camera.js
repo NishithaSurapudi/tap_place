c382325db04d81e8c798b8c01f6d7b7d5c785870
Nishitha <nishithasurapudi@gmail.com>
2020-06-23 12:19:40 +0530

    included pinch scale

--- camera.js
+++ camera.js
@@ -19,4 +19,33 @@
                     })
                 })
             }
-        })
+        });
+        AFRAME.registerComponent('pinch-scale', {
+            schema: {
+                min: { default: 5 },
+                max: { default: 100 }
+            },
+        
+            init: function () {
+                this.initialScale = this.el.object3D.scale.clone()
+                this.scaleFactor = 1
+                this.handleEvent = this.handleEvent.bind(this)
+                this.el.sceneEl.addEventListener('twofingermove', this.handleEvent)
+            },
+        
+            remove: function () {
+                this.el.sceneEl.removeEventListener('twofingermove', this.handleEvent)
+            },
+        
+            handleEvent: function (event) {
+                this.scaleFactor *= 1 + event.detail.spreadChange / event.detail.startSpread;
+                this.scaleFactor = Math.min(Math.max(this.scaleFactor, this.data.min), this.data.max);
+                var gltfModel;
+                if (clicked) {
+                    gltfModel = document.getElementById('Catapult');}
+        
+                gltfModel.object3D.scale.x = this.scaleFactor * this.initialScale.x;
+                gltfModel.object3D.scale.y = this.scaleFactor * this.initialScale.y;
+                gltfModel.object3D.scale.z = this.scaleFactor * this.initialScale.z;
+            }
+        });
