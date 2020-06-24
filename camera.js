26e3c05d31e599c582eb3992d52f50211869ef95
Nishitha <nishithasurapudi@gmail.com>
2020-06-24 10:33:09 +0530

    changed scale

--- camera.js
+++ camera.js
@@ -6,30 +6,27 @@
                 ground.addEventListener('click', event => {
                     // Create new entity for the new object
                     const newElement = document.createElement('a-entity')
-
                     // The raycaster gives a location of the touch in the scene
                     const touchPoint = event.detail.intersection.point
                     newElement.setAttribute('position', touchPoint)
                     newElement.setAttribute('visible', 'false')
-                    newElement.setAttribute('scale', '0.5 0.5 0.5')
+                    newElement.setAttribute('scale', '0.7 0.7 0.7')
                     newElement.setAttribute('gltf-model', '#Catapult')
                     this.el.sceneEl.appendChild(newElement)
                     newElement.addEventListener('model-loaded', () => {
                         newElement.setAttribute('visible', 'true')
                         //console.log('hi');
-                        newElement.setAttribute('animation', {
+                       /* newElement.setAttribute('animation', {
                             property: 'rotation',
                             from:'0 0 0',
                             to: '90 60 60',
                             //easing: 'easeOutElastic',
                             dur: 800,
                             property: 'scale',
-                            to: '7 7 7',
+                            to: '10 10 10',
                             easing: 'easeOutElastic',
                             dur: 800,
-
-                        })
-                        
+                        })*/
                     })
                 })
             }
