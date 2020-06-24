72e717f0a66653e2971779aff78eccec33b9a0cd
Nishitha <nishithasurapudi@gmail.com>
2020-06-24 15:11:23 +0530

    push

--- camera.js
+++ camera.js
@@ -9,8 +9,8 @@
                     // The raycaster gives a location of the touch in the scene
                     const touchPoint = event.detail.intersection.point
                     newElement.setAttribute('position', touchPoint)
-                    const randomYRotation = Math.random() * 360
-                    newElement.setAttribute('rotation', '0 ' + randomYRotation + ' 0')
+                    //const randomYRotation = Math.random() * 360
+                    //newElement.setAttribute('rotation', '0 ' + randomYRotation + ' 0')
                     newElement.setAttribute('visible', 'false')
                     newElement.setAttribute('scale', '0.05 0.05 0.05')
                     newElement.setAttribute('gltf-model', '#Catapult')
@@ -21,8 +21,8 @@
                        newElement.setAttribute('animation', {
                            // property: 'position',
                             //to: '20 20 20',
-                          property:'rotation',
-                           to:'45 45 45',
+                          //property:'rotation',
+                           //to:'45 45 45',
                             property: 'scale',
                             to: '0.5 0.5 0.5',
                             easing: 'easeOutElastic',
