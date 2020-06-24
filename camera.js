77955159a8785f9b8e9e3adf9b1984f6798cbaea
Nishitha <nishithasurapudi@gmail.com>
2020-06-24 15:40:10 +0530

    git

--- camera.js
+++ camera.js
@@ -12,7 +12,7 @@
                     //const randomYRotation = Math.random() * 360
                     //newElement.setAttribute('rotation', '0 ' + randomYRotation + ' 0')
                     newElement.setAttribute('visible', 'false')
-                    newElement.setAttribute('scale', '0.05 0.05 0.05')
+                    newElement.setAttribute('scale', '0.9 0.9 0.9')
                     newElement.setAttribute('gltf-model', '#Catapult')
                     this.el.sceneEl.appendChild(newElement)
                     newElement.addEventListener('model-loaded', () => {
