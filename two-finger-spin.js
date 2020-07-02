db871e14b9474f8b60fb75b78688c64769bde01e
Nishitha <nishithasurapudi@gmail.com>
2020-07-02 16:32:43 +0530

    gg

--- two-finger-spin.js
+++ two-finger-spin.js
@@ -5,9 +5,12 @@ AFRAME.registerComponent('two-finger-spin', {
     init: function() {
       this.handleEvent = this.handleEvent.bind(this)
       this.el.sceneEl.addEventListener('twofingermove', this.handleEvent)
+      this.el.sceneEl.addEventListener('onefingermove', this.handleEvent)
     },
     remove: function() {
       this.el.sceneEl.removeEventListener('twofingermove', this.handleEvent)
+      this.el.sceneEl.removeEventListener('onefingermove', this.handleEvent)
+
     },
     handleEvent: function(event) {
       this.el.object3D.rotation.y += event.detail.positionChange.x * this.data.factor
