a38decb31b6c4beb6cf3a7a1b7d6482183794fb1
Nishitha <nishithasurapudi@gmail.com>
2020-07-11 19:02:46 +0530

    add

--- js/camera.js
+++ js/camera.js
@@ -1,34 +1,52 @@
-        // Component that places model where the ground is clicked
-        //var arCamera = document.getElementById('aCamera');
-        AFRAME.registerComponent('tap-place', {
-            init: function() {
-                const ground = document.getElementById('ground')
-                ground.addEventListener('click', event => {
-                    // Create new entity for the new object
-                    const newElement = document.createElement('a-entity')
-                    // The raycaster gives a location of the touch in the scene
-                    const touchPoint = event.detail.intersection.point
-                    newElement.setAttribute('position', touchPoint)
-                    //const randomYRotation = Math.random() * 360
-                   // newElement.setAttribute('rotation', arCamera.components.rotation);
-                    newElement.setAttribute('visible', 'false')
-                    newElement.setAttribute('scale', '0.3 0.3 0.3')
-                    newElement.setAttribute('gltf-model', '#Catapult')
-                    this.el.sceneEl.appendChild(newElement)
-                    newElement.addEventListener('model-loaded', () => {
-                        newElement.setAttribute('visible', 'true')
-                        //console.log('hi');
-                        newElement.setAttribute('animation', {
-                          /*property: 'scale',
-                            to: '0.6 0.6 0.6',
-                            easing: 'easeOutElastic',
-                           dur: 800,
-                            property:'rotation',
-                            to:'45 45 45',
-                            loop:'true',
-                         dur:'29000'*/
-                          })
-                    })
+// Component that places model where the ground is clicked
+var isplaced = false;
+AFRAME.registerComponent('tap-place', {
+    init: function () {
+        const ground = document.getElementById('ground')
+        if (!isplaced) {
+            ispalced = true;
+            ground.addEventListener('click', event => {
+                // Create new entity for the new object
+                const newElement = document.createElement('a-entity')
+                const touchPoint = event.detail.intersection.point
+                newElement.setAttribute('position', touchPoint)
+                newElement.setAttribute('visible', 'false')
+                newElement.setAttribute('scale', '0.3 0.3 0.3')
+                newElement.setAttribute('id', 'MOdelID')
+                newElement.setAttribute('gltf-model', '#Catapult')
+                this.el.sceneEl.appendChild(newElement)
+                newElement.addEventListener('model-loaded', () => {
+                    newElement.setAttribute('visible', 'true')
                 })
-            }
-        })
+            })
+        }
+    }
+})
+
+AFRAME.registerComponent('pinch-scale', {
+    schema: {
+        min: { default: 0.3 },
+        max: { default: 8 }
+    },
+    init: function () {
+        this.initialScale = this.el.object3D.scale.clone()
+        this.scaleFactor = 1
+        this.handleEvent = this.handleEvent.bind(this)
+        this.el.sceneEl.addEventListener('twofingermove', this.handleEvent)
+    },
+    remove: function () {
+        this.el.sceneEl.removeEventListener('twofingermove', this.handleEvent)
+    },
+    handleEvent: function (event) {
+        this.scaleFactor *= 1 + event.detail.spreadChange / event.detail.startSpread
+        this.scaleFactor = Math.min(Math.max(this.scaleFactor, this.data.min), this.data.max)
+        var gltfModel;
+        if (isplaced) {
+            gltfModel = document.getElementById('modelID');
+        }
+
+        gltfModel.object3D.scale.x = this.scaleFactor * this.initialScale.x
+        gltfModel.object3D.scale.y = this.scaleFactor * this.initialScale.y
+        gltfModel.object3D.scale.z = this.scaleFactor * this.initialScale.z
+    }
+})
