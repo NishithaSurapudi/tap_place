e7203f0d565653a4d80b1971a6a1168f081e060f
Nishitha <nishithasurapudi@gmail.com>
2020-07-15 15:15:05 +0530

    git

--- js/camera.js
+++ js/camera.js
@@ -30,14 +30,8 @@ AFRAME.registerComponent('tap-place', {
 $(function () {
     $("#ground").click(function () {
         $("#start").fadeOut(6000)
-    });
-    setTimeout(() => {
-        $("#zoom").fadeIn(1000);
-        $("#placeholder").fadeIn("fast");
-    }, 1000);
-    $("ground").click(function(){
-        $("#zoom").fadeOut(1000);
-        $("#placeholder").fadeOut("fast");    
+        $(".pinchclass").fadeIn(fast);
+        $("#placeholder").fadeIn("fast"); 
     });
 });
 
